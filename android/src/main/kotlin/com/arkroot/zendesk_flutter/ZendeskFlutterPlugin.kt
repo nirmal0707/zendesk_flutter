893a578d7d0178c25da91b57f5cbe0f41e521d79
nirmal <nirmalkk0707@gmail.com>
2022-01-23 15:55:03 +0530

    add activity aware

--- android/src/main/kotlin/com/arkroot/zendesk_flutter/ZendeskFlutterPlugin.kt
+++ android/src/main/kotlin/com/arkroot/zendesk_flutter/ZendeskFlutterPlugin.kt
@@ -16,7 +16,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** ZendeskFlutterPlugin */
-class ZendeskFlutterPlugin : FlutterPlugin, MethodCallHandler {
+class ZendeskFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private val tag = "[ZendeskFlutterPlugin]"
 
     private lateinit var channel: MethodChannel
